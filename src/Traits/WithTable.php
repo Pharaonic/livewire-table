8cc5934206893174b93cd2dcc11d910416396afa
Moamen Eltouny <support@raggitech.com>
2021-08-28 19:55:14 +0200

    add pre ordering

--- src/Traits/WithTable.php
+++ src/Traits/WithTable.php
@@ -115,11 +115,8 @@ trait WithTable
         $this->options->set('paginate.current', $this->page);
 
         // Set Pre-Ordering
-        if ($this->options->get('order.status') && $this->options->get('order.column') && $this->columns->{$this->options->get('order.column')}) {
-            $direction = $this->options->get('order.direction', 'asc');
-            $this->options->set('order.direction', $direction);
-            $this->columns->{$this->options->get('order.column')}->setOrderDirection($direction);
-        }
+        if ($this->options->get('order.status') && $this->options->get('order.column') && $this->columns->{$this->options->get('order.column')} && !$this->orderColumn)
+            $this->setOrder($this->options->get('order.column'), $this->options->get('order.direction', 'asc'));
 
         // Load query builder with customized columns list.
         $this->builder = call_user_func_array([$this, 'query'], []);
